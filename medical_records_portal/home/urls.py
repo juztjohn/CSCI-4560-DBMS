81e309ec43b41bcaff2291c3231f6841444f9c40
jaron-mc <jaronbuckner@gmail.com>
2025-04-07 20:12:31 -0500

    Cleaned up file structure. Merged frontend/backend

--- medical_records_portal/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('patient/', views.patient, name='patient'),
-    path('appointments/', views.appointments_view, name = 'appointments'),
-]
